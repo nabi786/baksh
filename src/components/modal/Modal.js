6a9d255f3f23c971b7bec69e9d6a2ce82f7fc163
nabi786 <rehanpardesi2018@gmail.com>
2024-01-29 17:47:17 +0500

    updated code

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import { useContext } from 'react';
 import myContext from '../contextAPI/context'
 import { TextField } from '@mui/material';
-
+import PrimaryButton from '../button/primary'
 const style = {
   position: 'absolute',
   top: '50%',
@@ -16,16 +16,16 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
   boxShadow: 24,
   p: 4,
+  borderRadius : "10px"
 };
 
 export default function TransitionsModal() {
-    
-    let contextAPI = useContext(myContext)
 
-    console.log('this is context ', contextAPI)
+  let contextAPI = useContext(myContext)
+
+  console.log('this is context ', contextAPI)
 
   return (
     <div>
@@ -33,7 +33,7 @@ export default function TransitionsModal() {
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={contextAPI.contactModal}
-        onClose={()=>{contextAPI.setContactModal(false)}}
+        onClose={() => { contextAPI.setContactModal(false) }}
         closeAfterTransition
         slots={{ backdrop: Backdrop }}
         slotProps={{
@@ -44,11 +44,29 @@ export default function TransitionsModal() {
       >
         <Fade in={contextAPI.contactModal}>
           <Box sx={style}>
-           <form>
+            <Typography sx={
+              {
+                textAlign: "center",
+                fontWeight: "bold",
+                fontSize: "25px"
+              }
+            }>
+              Contact
+            </Typography>
+            <form style={
+              {
+                margin: "10px 0px 0px 0px"
+              }
+            }>
+              <TextField fullWidth form label="name" sx={{marginTop : "10px"}}/>
+              <TextField fullWidth label="email" sx={{marginTop : "10px"}} />
+              <TextField multiline rows={4} fullWidth label="message" sx={{marginTop : "10px"}} />
 
-              <TextField label="name"/>
-              <TextField label="email"/>
-              <TextField label="message"/>
+             
+
+              <Box sx={{marginTop : "10px"}}>
+                <PrimaryButton name="Submit"  />
+              </Box>
             </form>
           </Box>
         </Fade>
